49cfb5ca3c4f4ac7f8ef45488c1d09731aabbd60
Davaabayar <busdavaabayar@gmail.com>
2019-10-19 12:25:51 -0500

    Standard solution

---
+++ src/main/java/edu/mum/cs/cs425/finals/adidasler/service/AthleteService.java
@@ -0,0 +1,12 @@
+package edu.mum.cs.cs425.finals.adidasler.service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import edu.mum.cs.cs425.finals.adidasler.model.Athlete;
+
+public interface AthleteService {
+	public abstract List<Athlete> getAllAthletes();
+	public abstract Athlete addAthlete(Athlete athlete);
+	public abstract List<Athlete> getElite(LocalDate date);
+}
